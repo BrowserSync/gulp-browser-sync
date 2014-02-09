7612443f9b859748aab29fea85875f69a023349c
Shane Osbourne <shane.osbourne8@gmail.com>
2014-02-09 18:15:13 +0000

    Add example gulpfiles

---
+++ gulpfile-example-01.js
@@ -0,0 +1,30 @@
+/**
+ * This example:
+ *  Uses the built-in BrowserSync server for HTML files
+ *  Watches & compiles SASS files
+ *  Watches & injects CSS files
+ */
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var sass = require('gulp-sass');
+
+// Browser-sync task, only cares about compiled CSS
+gulp.task('browser-sync', function() {
+    browserSync.init("css/*.css", {
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+// Sass task, will run when any SCSS files change.
+gulp.task('sass', function () {
+    gulp.src('scss/styles.scss')
+        .pipe(sass({includePaths: ['scss']}))
+        .pipe(gulp.dest('css'));
+});
+
+// Default task to be run with `gulp`
+gulp.task('default', ['sass', 'browser-sync'], function () {
+    gulp.watch("scss/*.scss", ['sass']);
+});
---
+++ gulpfile-example-API.js
@@ -0,0 +1,33 @@
+/**
+ * For the more complex/customised workflows, you may want to handle the file-change
+ * events manually. This following example is simplistic - but shows how can emit your events
+ * to BrowserSync whenever you like.
+ *
+ * This example:
+ *   Shows how you can emit your own file-change events
+ */
+
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var sass = require('gulp-sass');
+
+var bs;
+
+// This BrowserSync task will start a server, but will NOT watch any files.
+gulp.task('browser-sync', function () {
+    bs = browserSync.init(null, {
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+// This default task will run BrowserSync & then use Gulp to watch files.
+// When a file is changed, an event is emitted to BrowserSync with the filepath.
+gulp.task('default', ['browser-sync'], function () {
+    gulp.watch('css/*.css', function (file) {
+        if (file.type === "changed") {
+            bs.emit("file:changed", {path: file.path});
+        }
+    });
+});
