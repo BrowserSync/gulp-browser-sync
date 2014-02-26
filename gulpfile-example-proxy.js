d2dc9d6d058a1f050433c39bbc6e375dd0a65835
Shane Osbourne <shane.osbourne8@gmail.com>
2014-02-26 22:05:46 +0000

    Add proxy example

---
+++ gulpfile-example-proxy.js
@@ -0,0 +1,19 @@
+/**
+ * This example:
+ *  Shows how to plug in the details of your server.
+ *  Watches & injects CSS files
+ */
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+
+gulp.task('browser-sync', function() {
+    browserSync.init("css/*.css", {
+        proxy: {
+            host: "mylocal.dev",
+            port: 8080
+        }
+    });
+});
+
+// Default task to be run with `gulp`
+gulp.task('default', ["browser-sync"]);
