93157a14e44110b8c094ee22d41bcad83e5549f6
Shane Osbourne <shane.osbourne8@gmail.com>
2014-02-26 22:07:16 +0000

    Add server example

---
+++ gulpfile-example-server.js
@@ -0,0 +1,18 @@
+/**
+ * This example:
+ *  Shows how to use the built-in server, by default it
+ *  Watches & injects CSS files
+ */
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+
+gulp.task('browser-sync', function() {
+    browserSync.init("css/*.css", {
+        server: {
+            baseDir: "app" // Change this to your web root dir
+        }
+    });
+});
+
+// Default task to be run with `gulp`
+gulp.task('default', ["browser-sync"]);
